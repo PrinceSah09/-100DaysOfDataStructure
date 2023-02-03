a905c2b13efd11a88d160a928932f7c2f82e65be
Prince <81484022+PrinceSah09@users.noreply.github.com>
2023-02-03 18:28:21 +0530

    367. Valid Perfect Square
    Given a positive integer num, return true if num is a perfect square or false otherwise.
    
    A perfect square is an integer that is the square of an integer. In other words, it is the product of some integer with itself.
    
    You must not use any built-in library function, such as sqrt.

---
+++ DAY-17-(Q1).cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool isPerfectSquare(int num) {
+
+        //Using Binary Search
+        int s = 1 , e =num;
+        while(s<=e){
+            long long int mid = s+(e-s)/2;
+            long long int sq = mid*mid; 
+            if(sq == num)return true;
+            else if(sq > num) e = mid-1;
+            else{
+                s = mid+1;
+            }
+        }
+        return false;
+
+        // Brute Force
+        // for(long long int i=1;i<=num;i++){
+        //     if(i*i==num){
+        //         return true;
+        //     }
+        // }
+        // return false; 
+    }
+};
